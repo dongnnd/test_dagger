56614c7935848bbe9e105218b864bd8ca09a4fc8
DongND <dongnguyen9494@gmail.com>
2021-02-16 17:09:39 +0700

    [CL] Implement main and registration flow

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,4 +1,23 @@
 package com.example.android.dagger.di
 
-class AppComponent {
+import android.content.Context
+import com.example.android.dagger.login.LoginActivity
+import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.registration.RegistrationActivity
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [StorageModule::class])
+interface AppComponent {
+
+    @Component.Factory
+    interface Factory{
+        fun create(@BindsInstance context: Context) : AppComponent
+    }
+
+    fun inject(activity: RegistrationActivity)
+    fun inject(activity: MainActivity)
+    fun inject(activity: LoginActivity)
 }
